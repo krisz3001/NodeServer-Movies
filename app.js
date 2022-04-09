a8ce08700bff1c3e1a6bfa0aa3cdba98abd017fc
krisz3001 <horvath.krisztian3001@gmail.com>
2022-04-10 00:15:28 +0200

    new design

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ var express = require('express');
 var app = express();
 var fs = require('fs');
 var bodyParser = require('body-parser');
+const { json } = require('express/lib/response');
 
 function time(){
   let date = new Date();
@@ -18,7 +19,7 @@ const logName = 'logs/' + launchTime + '_log.txt'
 function saveLog(log){
   if(!fs.existsSync(__dirname + '/logs')){
     fs.mkdirSync(__dirname + '/logs');
-    console.log(time() + 'Log directory created.');
+    saveLog('Log directory created.')
     fs.appendFileSync(logName, sessionStart, function(err){
       if(err) throw err;
     });
@@ -44,12 +45,12 @@ function saveLog(log){
       });
     }
   }
-  return log;
+  console.log(time() + log)
 }
 //media mappa létrehozása
 if(!fs.existsSync(__dirname + '/media')){
-  fs.mkdirSync(__dirname + '/media');
-  console.log(saveLog(time() + 'Media folder created.'));
+  fs.mkdirSync(__dirname + '/media')
+  saveLog('Media folder created.')
 }
 
 app.set('view engine', 'ejs');
@@ -58,193 +59,45 @@ app.use('/assets', express.static('assets'));
 var urlencodedParser = bodyParser.urlencoded({ extended: false })
 
 //adatbázis olvasás
+let br = __dirname + '/br.txt'
 if(fs.existsSync(__dirname + '/db.json')){
   var raw = fs.readFileSync('db.json');
   var db = JSON.parse(raw);
 } else{
   var dbname = __dirname + '/db.json';
-  var init_array = {"series":[],"movies":[]};
+  var init_array = [];
   var init_json = JSON.stringify(init_array, null, '\t');
   fs.appendFileSync(dbname, init_json, function(err){
     if(err) throw err;
   });
-  console.log(saveLog(time() + 'db.json file created.'));
+  saveLog('db.json file created.')
   var raw = fs.readFileSync('db.json');
   var db = JSON.parse(raw);
 }
-
-app.post('/', urlencodedParser, function(req, res) {
-  var fullname = req.body.fullname;
-  var name = req.body.name;
-  if(req.body.type === 'movies'){
-    var pattern_movies = {"name":name,"fullname":fullname};
-    var movie_exists = false;
-    var ip = req.ip.substr(7);
-	if(ip === '') ip = 'Server'
-    for(var i in db.movies){
-      if(db.movies[i].name === name){
-        movie_exists = true;
-        break;
-      }
-    }
-    if(movie_exists === false){
-      save_movies(pattern_movies);
-      console.log(saveLog(time() + 'Movie (' + fullname + '; ' + name + ') was added by ' + ip));
-    }
-  } else if (req.body.type === 'series'){
-    var s = req.body.s;
-    var e = req.body.e;
-    var pattern_series = {"name":name,"fullname":fullname,"s":s,"e":e};
-    var series_exists = false;
-    for(var i in db.series){
-      if(db.series[i].name === name){
-        series_exists = true;
-        break;
-      }
-    }
-    if(series_exists === false){
-      save_series(pattern_series);
-      var ip = req.ip.substr(7);
-      console.log(saveLog(time() + 'Series (' + fullname + '; ' + name + '; ' + 'S:' + s + '; E:' + e + ') was added by ' + ip));
-    }
+//Battle Royale
+app.post('/br', urlencodedParser, function(req, res) {
+  let txt
+  try {
+    txt = {"count":JSON.parse(fs.readFileSync('br.txt')).count+1}
+  } catch (error) {
+    txt = {"count":1}
+    fs.writeFileSync(br, JSON.stringify(txt, null, '\t'))
   }
-  res.render('home', db);
-});
-
-//sorozat mentése
-function save_series(obj){
-  db.series.push(obj);
-  var savefile = JSON.stringify(db, null, "\t");
-  fs.writeFile('db.json', savefile, 'utf8', function(err, data){
-    if(err){
-      console.log(err);
-    }
-  });
-}
-
-//film mentése
-function save_movies(obj){
-  db.movies.push(obj);
-  var savefile = JSON.stringify(db, null, "\t");
-  fs.writeFile('db.json', savefile, 'utf8', function(err, data){
-    if(err){
-      console.log(err);
-    }
-  });
-}
-
-//sorozat törlése
-function delete_series(name, ip){
-  var fullname = '';
-  var s = '';
-  var e = '';
-  for(var i in db.series){
-    if(name === db.series[i].name){
-      fullname = db.series[i].fullname;
-      s = db.series[i].s;
-      e = db.series[i].e;
-      delete db.series[i];
-    }
-  }
-  var clean_db = db.series.filter(function (elem){
-    return elem != null;
-  });
-  db.series = clean_db;
-  var savefile = JSON.stringify(db, null, '\t');
-  fs.writeFile('db.json', savefile, 'utf8', function(err, data){
-    if(err){
-      console.log(err);
-    }
-  });
-  console.log(saveLog(time() + 'Series (' + fullname + '; ' + name + '; ' + 'S:' + s + '; E:' + e + ') was deleted by ' + ip));
-}
-//returns fullname
-function full(name){
-  var movie = false;
-  for(var i in db.movies){
-    if(db.movies[i].name === name){
-      return db.movies[i].fullname;
-      movie = true;
-      break;
-    }
-  }
-  if(movie === false){
-    for(var i in db.series){
-      if(db.series[i].name === name){
-        return db.series[i].fullname;
-        break;
-      }
-    }
-  }
-}
-//film törlése
-function delete_movies(name, ip){
-  var fullname = '';
-  for(var i in db.movies){
-    if(name === db.movies[i].name){
-      fullname = db.movies[i].fullname;
-      delete db.movies[i];
-    }
-  }
-  var clean_db = db.movies.filter(function (elem){
-    return elem != null;
-  });
-  db.movies = clean_db;
-  var savefile = JSON.stringify(db, null, '\t');
-  fs.writeFile('db.json', savefile, 'utf8', function(err, data){
-    if(err){
-      console.log(err);
-    }
-  });
-  console.log(saveLog(time() + 'Movie (' + fullname + '; ' + name + ') was deleted by ' + ip));
-}
-
-app.get('/', function(req, res) {
-  res.render('home', db);
-});
-app.get('*', function(req, res){
+  fs.writeFileSync(br, JSON.stringify(txt, null, '\t'))
+  res.send(txt)
   var ip = req.ip.substr(7);
   if(ip === '') ip = 'Server'
-  if(req.url[1] === '&'){
-    var fullname = full(req.url.substr(2));
-    res.render('series', {name: req.url.substr(2), db, fullname});
-  } else if(req.url[1] === '-'){
-    if(req.url[2] === 'm'){
-      delete_movies(req.url.substr(4), ip);
-    } else if (req.url[2] === 's') {
-      delete_series(req.url.substr(4), ip);
-    }
-    res.render('delete', db);
-  } else if(req.url[1] === '!'){
-    if(fs.existsSync(__dirname + '/media/' + req.url.substr(2) + '.mp4')){
-      if(req.url.includes('_')){
-      var fullname = full(req.url.substr(req.url.indexOf('!') + 1, req.url.indexOf('_')-2));
-      var temp_s = req.url.substr(req.url.lastIndexOf('s'));
-      if(temp_s[2] === '_'){
-        var s = req.url.substr(req.url.lastIndexOf('s')+1, 1);
-      } else{
-        var s = req.url.substr(req.url.lastIndexOf('s')+1, 2);
-      }
-      var temp_e = req.url.substr(req.url.lastIndexOf('e'));
-      if(temp_e[2] === '_'){
-        var e = req.url.substr(req.url.lastIndexOf('e')+1, 1);
-      } else{
-        var e = req.url.substr(req.url.lastIndexOf('e')+1, 2);
-      }
-    }
-      else{
-        var fullname = full(req.url.substr(2));
-      }
-      var media_name = req.url.substr(2) + '.mp4';
-      res.render('media', {name: req.url.substr(2), dir: '/media/' + req.url.substr(2) + '.mp4', fullname, s, e});
-      console.log(saveLog(time() + ip + ' asked for ' + media_name));
-      if(s === undefined || e === undefined)
-      console.log(saveLog(time() + ip + ' is watching ' + fullname));
-      else
-      console.log(saveLog(time() + ip + ' is watching ' + fullname + ' S' + s + ' E' + e));
-    }else
-      res.render('404');
-  }
+  else if(ip === '100.70.64.221') ip = 'Bulazs'
+  saveLog(ip + " baited.")
 });
+app.get('/', function(req, res) {
+  res.render('home', db);
+  var ip = req.ip.substr(7);
+  if(ip === '') ip = 'Server'
+  else if(ip === '100.70.64.221') ip = 'Bulazs'
+});
+app.get('/db.json', function(req, res){
+  res.send(db)
+})
 app.listen(3001);
-console.log(saveLog(time() + 'Listening to port 3001...'));
+saveLog('Listening to port 3001...')
--- assets/script.js
+++ assets/script.js
@@ -1,6 +1,22 @@
 function expand(){
-    document.getElementById('a').innerHTML += document.getElementById('a').innerHTML
+    document.getElementById('items').innerHTML += `<div class="col-sm-4 col-lg-3 col-6 p-0" id="item"><img src="assets/placeholder.jpg" class="img-fluid border" id="item-img"><div class="d-none bg-dark p-2 border w-100" id="item-info"><p class="h5" id="item-title">Title</p><input type="button" value="Play" class="btn btn-success w-100"><p class="text-justify m-0" id="item-desc">Description</p></div></div>`
 }
 function info(e){
     e.innerHTML += '<p class="text-white"></p>'
+}
+fetch('/db.json')
+.then(response => response.json())
+.then(r => {
+    for (let i = 0; i < r.length; i++) {
+        document.getElementById('items').innerHTML += `<div class="col-sm-4 col-lg-3 col-6 p-0" id="item"><img src="assets/${r[i].image}" class="img-fluid border"><div id="item-info" class="d-none bg-dark p-2 border w-100"><p class="h5">${r[i].title}</p><input type="button" value="Play" class="btn btn-success w-100"><p class="text-justify m-0">${r[i].description}</p></div></div>`
+    }
+})
+function br(){
+      fetch("/br", {method: 'POST', redirect: 'follow'})
+        .then(response => response.json())
+        .then(r => {
+            let a = document.getElementById('br_count')
+            a.innerHTML = `Count: ${r.count}`
+            a.style = 'display: block!important'
+        })
 }
